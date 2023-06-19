7d6fde5ad8f2459e4ecec15ed936cec2d0abe77e
Gilvan Pires <gilvan.trial928@startmail.com>
2023-06-19 19:57:53 -0300

    Gerador de senhas.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('Ralou word');
+import { nanoid } from 'nanoid';
+import './style.css'; // É o vite que permite importar css dentro do js.
+
+const pwBtnElmt = document.querySelector('button');
+// console.log(pwBtnElmt);
+const displayPwElmt = document.querySelector('h2');
+
+pwBtnElmt.addEventListener('click', () => {
+  const randomPw = nanoid();
+  displayPwElmt.innerHTML = randomPw;
+});
