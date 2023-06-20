43313aa963641b70d4aad304646a3f9815ea726e
Gilvan Pires <gilvan.trial928@startmail.com>
2023-06-20 09:48:15 -0300

    Introduz bibliotecas do clipboard-copy.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { nanoid } from 'nanoid';
 import './style.css'; // É o vite que permite importar css dentro do js.
+
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 
 const pwBtnElmt = document.querySelector('button');
 // console.log(pwBtnElmt);
@@ -8,4 +10,9 @@ const displayPwElmt = document.querySelector('h2');
 pwBtnElmt.addEventListener('click', () => {
   const randomPw = nanoid();
   displayPwElmt.innerHTML = randomPw;
+});
+
+displayPwElmt.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Password copied!');
 });
